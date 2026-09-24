de060b20d0ceaecfac6f8a6b0bcf9d4cc6d0a37a
Daniel Medina <daniel@medina.com>
2026-09-24 14:13:49 -0600

    feat: remove print

--- src/caritas_backend/__main__.py
+++ src/caritas_backend/__main__.py
@@ -5,9 +5,7 @@ from .app import create_app
 
 def main() -> None:
     app = create_app()
-
-    print(os.getcwd())
-
+    
     # Update these paths to point to your SSL certificate and key files
     cert_path = os.getcwd() + "/src/caritas_backend/cert-equipo/minihub.tc2007b.tec.mx.cer"
     key_path = os.getcwd() + "/src/caritas_backend/cert-equipo/minihub.tc2007b.tec.mx.key"

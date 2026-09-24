b00ada6e85f43e6d0e777f3a33397b001293669e
Daniel Medina <daniel@medina.com>
2026-09-24 09:11:22 -0600

    feat: added CRUD routes for 'Causas'

--- src/caritas_backend/routes.py
+++ src/caritas_backend/routes.py
@@ -3,6 +3,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 from .modules.app.controller import blueprint as app_blueprint
 from .modules.auth.controller import blueprint as auth_blueprint
+from .modules.causas.controller import blueprint as causas_blueprint
 from .modules.donor.controller import blueprint as donors_blueprint
 from .modules.historial.controller import blueprint as history_blueprint
 from .modules.llamadas.controller import blueprint as calls_blueprint
@@ -15,6 +16,7 @@ def register_routes(app: Flask) -> None:
     app.register_blueprint(app_blueprint)
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(calls_blueprint)
+    app.register_blueprint(causas_blueprint)
     app.register_blueprint(donors_blueprint)
     app.register_blueprint(history_blueprint)
 

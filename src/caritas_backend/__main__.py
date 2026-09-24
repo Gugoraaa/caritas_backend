9443951ea0da6051beb251decb21eedc5360d03d
Daniel Medina <daniel@medina.com>
2026-09-24 14:07:07 -0600

    feat: added SSL

--- src/caritas_backend/__main__.py
+++ src/caritas_backend/__main__.py
@@ -4,8 +4,17 @@ from .app import create_app
 
 
 def main() -> None:
-    app = create_app()  
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT") or 3000))
+    app = create_app()
+
+    # Update these paths to point to your SSL certificate and key files
+    cert_path = "/home/user01/mnt/caritas_backend/src/caritas_backend/cert-equipo/minihub.tc2007b.tec.mx.cer"
+    key_path = "/home/user01/mnt/caritas_backend/src/caritas_backend/cert-equipo/minihub.tc2007b.tec.mx.key"
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT") or 3000),
+        ssl_context=(cert_path, key_path),
+    )
 
 
 if __name__ == "__main__":
